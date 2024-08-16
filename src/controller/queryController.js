ee38552a411544cd1512ac644e569feb93a79023
Talita <silveira.tacristina@gmail.com>
2024-08-15 23:27:45 -0300

    chore: live-alunos

--- src/controller/queryController.js
+++ src/controller/queryController.js
@@ -4,26 +4,6 @@ const path = require("path");
 
 class QueryController {
   constructor() {}
-
-  async index(req, res, next) {
-    return res.status(200).send(tasks.tasks);
-  }
-
-  async getTaskById(req, res, next) {
-    const id = parseInt(req.params.id, 10);
-
-    if (isNaN(id)) {
-      return res.status(400).send("Invalid ID format");
-    }
-
-    const task = tasks.tasks.find((t) => t.id === id);
-
-    if (!task) {
-      return res.status(404).send("Task not found");
-    }
-
-    return res.status(200).send(task);
-  }
 }
 
 module.exports = () => new QueryController();
--- src/router.js
+++ src/router.js
@@ -5,10 +5,5 @@ const command = commandController();
 const query = queryController();
 
 const router = express.Router();
-router.post("/tasks", command.createTask.bind(command));
-router.put("/tasks/:id", command.updateTask.bind(command));
-router.delete("/tasks/:id", command.deleteTask.bind(command));
 
-router.get("/tasks", query.index.bind(query));
-router.get("/tasks/:id", query.getTaskById.bind(query));
 module.exports = { router };
