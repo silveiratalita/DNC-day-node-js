65bfe773a6d080de5b736d59dfe2dbdba828f7fa
Talita <silveira.tacristina@gmail.com>
2024-08-15 23:03:54 -0300

    feat: start class

--- books.js
+++ books.js
@@ -1,33 +1,26 @@
 module.exports = {
-  "ids": 7,
-  "books": [
+  ids: 7,
+  books: [
     {
-      "id": 1,
-      "title": "O Senhor dos Anéis",
-      "author": "J.R.R. Tolkien",
-      "year": 1954,
-      "genre": "Fantasia"
+      id: 1,
+      title: "O Senhor dos Anéis",
+      author: "J.R.R. Tolkien",
+      year: 1954,
+      genre: "Fantasia",
     },
     {
-      "id": 2,
-      "title": "1984",
-      "author": "George Orwell",
-      "year": 1949,
-      "genre": "Distopia"
+      id: 2,
+      title: "1984",
+      author: "George Orwell",
+      year: 1949,
+      genre: "Distopia",
     },
     {
-      "id": 3,
-      "title": "Dom Quixote",
-      "author": "Miguel de Cervantes",
-      "year": 1605,
-      "genre": "Clássico"
+      id: 3,
+      title: "Dom Quixote",
+      author: "Miguel de Cervantes",
+      year: 1605,
+      genre: "Clássico",
     },
-    {
-      "id": 7,
-      "title": "Contos de TERROR2tt",
-      "author": "Irmão Grimm",
-      "year": 1817,
-      "genre": "Clássico"
-    }
-  ]
+  ],
 };
--- src/router.js
+++ src/router.js
@@ -5,10 +5,5 @@ const command = commandController();
 const query = queryController();
 
 const router = express.Router();
-router.post("/create", command.createBook.bind(command));
-router.put("/update/:id", command.updateBook.bind(command));
-router.delete("/delete/:id", command.deleteBook.bind(command));
 
-router.get("/books", query.index.bind(query));
-router.get("/books/:id", query.getBookById.bind(query));
 module.exports = { router };
